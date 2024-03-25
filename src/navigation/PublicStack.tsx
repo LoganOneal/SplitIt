e66d8fa7d3724e9191bd228b00471cce7909c800
loganoneal <loganofneal@gmail.com>
2024-03-25 10:17:45 -0400

    remove splash screen

--- src/navigation/PublicStack.tsx
+++ src/navigation/PublicStack.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignInScreen from "../screens/SignInScreen";
-import SplashScreen from "../screens/SplashScreen";
 import * as AppConstants from "../constants/constants";
 import SignUpScreen from "../screens/SignUpScreen";
 import ThemeToggle from "./ThemeToggle";
@@ -17,14 +16,13 @@ export default function PublicStack() {
   /* Conditional stack navigator rendering based on login state */
   return (
     <Stack.Navigator
-      initialRouteName="Splash"
+      initialRouteName="SignIn"
       screenOptions={{
         headerShown: false,
       }}
     >
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
     </Stack.Navigator>
   );
